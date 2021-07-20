7c193e62d610c013f5f75f9b4e0ea97c67b06c26
koolice19 <mckoolice@gmail.com>
2021-07-19 20:08:46 -0700

    first push

---
+++ lib/dummy_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Abs',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Chest',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Legs',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Arms',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Back',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Cardio',
+    color: Colors.green,
+  ),
+];
